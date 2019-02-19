9e140a59c605eca1dcdc32d9f8cb2eb67d84dca9
lucavehbiu <lucavehbiu96@gmail.com>
2019-02-19 09:50:46 +0100

    Update GeShinyApp.R

--- GeShinyApp.R
+++ GeShinyApp.R
@@ -4,9 +4,20 @@ library(shinythemes)
 library(plotly)
 
 
-nms <- names(test)
+#list of all variables
+nms <- names(final_data)
 
-nmss <- as.vector(pjes$Gemeentena)
+#list of municipalities names
+nmss <- as.vector(final_data$Gemeentena)
+
+#list of region names
+region <- as.vector(final_data$Res_regio)
+
+#list of projection year variables
+year <- c("baseYearAO", "prj15AO",  "prj20AO",  "prj25AO", "prj30AO",  "prj40AO", "prj50AO")
+
+
+
 
 #UI Interface ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -15,21 +26,28 @@ ui <- dashboardPage(
                     #UI header
                     
                     dashboardHeader(
-                        title = "Energy Distribution - All administrative levels"),
+                        title = "Energy Demand Potential",
+                        tags$li(a(href = 'http://generation.energy/',
+                                  img(src = '0.jpg',
+                                      title = "Generation.Energy", height = "30px")),
+                                class = "dropdown")),
 
                     #Dashboard sidebar
                     dashboardSidebar(
-                      
-                        selectInput(inputId = "citySelection",
-                                    label = "Select city:",
-                                    choices = sort(unique(nmss))),
-                        selectInput('color', 'Color', choices = nms, selected = "AO40"),
+                        selectInput('region', 
+                                    'Select Region:',
+                                    choices = sort(region),
+                                    selected = "`Regio Wes Brabant`"),
                         
                         selectInput(inputId = "yearSelection",
-                                    label = "Select year",
+                                    label = "Select Projection year:",
                                     choices = sort(nms)),
                         
-                        htmlOutput(outputId = "projFilter")
+                        htmlOutput(outputId = "regioServer")
+                        #htmlOutput(outputId = "yearSelection")
+                        
+               
+                        
                         
                         
                       ),
@@ -57,51 +75,67 @@ server <- function(input, output) {
   
   #Server inputs
   
-  output$projFilter <- renderUI({
+  output$regioServer <- renderUI({
     
-    #data <- portion %>% gather(ends_with("50AO_tj"), key = "AO50", value = "prj50AO") %>% 
-      #filter(Gemeentenaam == input$citySelection)
+    ultimo <- final_data %>% filter(Res_regio == input$region)
     
-    selectInput('y', 'Y', choices = lali, selected = "prj40AO")
+    selectInput(inputId = "citySelection",
+                label = "Select city from selected Region:",
+                choices = sort(ultimo$Gemeentena))
     
-    
+
+
     
   })
-  
   
   #plots
   
   output$plot2 <- renderPlot({
     
     #portion %>% gather(ends_with("50AO_tj"), key = "AO50", value = "prj50AO") %>% 
-    test %>%   
-      filter(Gemeentena == input$citySelection) %>% 
+    g2 <- final_data %>%   
+      filter(Res_regio == input$region) %>% 
+      group_by(Res_regio) %>% 
       mutate(fraction = prj40AO / sum(prj40AO)) %>% 
-      arrange(fraction) %>% 
       mutate(ymax = cumsum(fraction),
              ymin = c(0, head(ymax, n = -1))) %>% 
       ggplot(aes(fill = AO40, ymax = ymax, ymin = ymin, xmax = 4, xmin = 3)) +
       geom_rect() +
       coord_polar(theta = "y") +
       xlim(c(0, 4)) +
-      labs(y = element_blank(), x = input$citySelection, title = "Energy Potential Distribution (TJ) - 2050 AO") +
+      labs(y = element_blank(), x = element_blank()) +
       guides(fill = guide_legend(title = "Energy sources")) +
-      theme(axis.ticks = element_blank(), axis.text = element_blank(), panel.background = element_blank()) +
-      scale_fill_brewer(palette = "Spectral")
+      theme(axis.ticks = element_blank(), axis.text = element_blank(), 
+            panel.background = element_blank()) +
+      scale_fill_brewer(palette = "Set3")
+    
+    tg2 <- grobTree(
+                   textGrob(input$region, 
+                            y= -0.3, 
+                            vjust= 0.8, 
+                            gp = gpar(fontsize= 40, face=4, col="gray")),
+                   cl="titlegrob")
+    
+    heightDetails.titlegrob <- function(x) 
+      do.call(sum,lapply(x$children, grobHeight))
+    
+    
+    grid.arrange(g2, top = tg2)
+    
     
     
     
     
   })
   
-  
+#Bar chart  
   output$plot3 <- renderPlot({
-    pjes %>% gather(ends_with("40AO_tj"), key = "AO40", value = "prj40AO") %>% 
-      select(Gemeentena, prj40AO, AO40) %>% 
-      filter(Gemeentena == input$citySelection) %>% 
-      mutate(fraction = round((prj40AO/ sum(prj40AO)), digits = 2)) -> test
     
-  g <- test %>%   ggplot(aes(Gemeentena, fraction, fill = AO40)) +
+  g <- final_data %>% 
+    filter(Res_regio == input$region,
+      Gemeentena == input$citySelection) %>% 
+    mutate(fraction = round((prj40AO/ sum(prj40AO)), digits = 2) *100) %>% 
+    ggplot(aes(Gemeentena, fraction, fill = AO40)) +
       geom_bar(stat = "identity", position = "dodge") +
       labs(
         x = element_blank(), 
@@ -113,17 +147,24 @@ server <- function(input, output) {
       scale_fill_brewer(palette = "Spectral") + 
       geom_text(aes(label = paste0(fraction, '%')), 
                 position=position_dodge(width=0.9), 
-                vjust= -0.2,
-                data = subset(test, fraction > 0)) +
-      scale_y_continuous(labels = percent) +
+                vjust= -0.2) +
+      scale_y_continuous() +
     coord_flip()
     
-    tg <- grobTree(textGrob("Energy Potential Distribution (TJ) - 2050 AO", 
-                            y= 0.8, vjust=1, gp = gpar(fontsize=13, face=2, col="gray")),
-                   textGrob(input$citySelection, y= -0.3, vjust= 0, gp = gpar(fontsize= 40, face=4, col="gray")),
+    tg <- grobTree(textGrob(input$region, 
+                            y= 0.8, 
+                            vjust= 0.8, 
+                            gp = gpar(fontsize=17, 
+                                      face=2, 
+                                      col="lightblue")),
+                   textGrob(input$citySelection, 
+                            y= -0.3, 
+                            vjust= 1.2, 
+                            gp = gpar(fontsize= 40, face=4, col="gray")),
                    cl="titlegrob")
     
-    heightDetails.titlegrob <- function(x) do.call(sum,lapply(x$children, grobHeight))
+    heightDetails.titlegrob <- function(x) 
+      do.call(sum,lapply(x$children, grobHeight))
     
     
     grid.arrange(g, top = tg)

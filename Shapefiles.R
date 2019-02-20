ee994fd71721c59cbdb303437a805f569d8bee09
lucavehbiu <lucavehbiu96@gmail.com>
2019-02-20 17:15:23 +0100

    Update Shapefiles.R

--- Shapefiles.R
+++ Shapefiles.R
@@ -1,23 +1,46 @@
-require(sf)
-require(rgdal)
-require(maptools)
-require(ggmap)
 
 #working with shapefiles R
-shape <- readOGR(dsn = ".", layer = "zon_pot_heide_akl_grl_10p")
+area <- readOGR(dsn = ".", layer = "RES_regios_2019")
 
-shape2 <- readOGR(dsn = ".", layer = "zon_dak_pot1")
+#fortify dat little biatch
+area.fort <- fortify(area)
 
-#find max area
-maxArea <- max(shape2$shape_area)
+#merge with area pints 
+#(putting regions and geementes in the dat of the shapefile)
+area@data$id <- rownames(area@data)
+area.points <- fortify(area, coords="id")
+area.fort <- plyr:: join(area.points, area@data, by="id")
 
-biggestAreaMask <- which(shape2$shape_area == maxArea)
-biggestAreaName <- shape2$name[biggestAreaMask]
+#try plotting
+ggplot(area.fort, aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(x = long, y = lat, group = group, fill = c(Res_regio, Gemeentena) == mapFill)) +
+  geom_path() +
+  coord_equal() +
+  guides(fill = guide_legend(element_blank())) +
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.background = element_blank())
+  
 
-area <- fortify(shape2)
+plot(area, cex = plotrix:::rescale(area@data$Shape__Len, c(1, 4)), pch = 19)
 
 
-allAreas <- spplot(shape2@polygons, zcol = "area", identify = T)
+mapFill <- list("Metropoolregio Eindhoven", "Eindhoven")
 
 
-bashk %>% ggplot(aes(Shape__Are, Shape__Len, color = Res_regio)) + geom_point()
+
+require(plotly)
+devtools::install_github('ropensci/plotly')
+
+class(area.fort)
+
+
+
+
+
+
+
+
+
+
+

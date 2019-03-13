541938adad2879d8407334e5d1d6b007312efeb2
lucavehbiu <lucavehbiu96@gmail.com>
2019-03-13 19:39:25 +0100

    Update NaturalResources.R

--- NaturalResources.R
+++ NaturalResources.R
@@ -1,18 +1,17 @@
 #natural resources and maps
+nature <- import("natural_resources.csv", skip = 1)
 
-nature <- import("natural_resources.csv")
-
-#############
-header.true <- function(df) {
-  names(df) <- as.character(unlist(df[1,]))
-  df[-1,]
-}
+#Function for making the first row the header
+load("header.true.R")
 header.true(nature) -> nature #put the right header
+
+#recode variables the same as in the working horse(data)
 nature <- plyr:: rename(nature, c("Regio's" = "Gemeentena",
                                 "2015" = "surface",
                                 "Onderwerp" = "resource")) #rename columns
 nature <- repair_names(nature) #repair names
 
+#make the names pretty
 nature <- nature %>% filter(!(resource == "Totale oppervlakte" |
                               resource == "||Totaal buitenwater")) #remove unnecessary stuff
 
@@ -86,4 +85,3 @@ nature$Gemeentena <- recode(nature$Gemeentena,
                            'Vlagtwedde' = 'Westerwolde')
 
 
-#############  
